13dbe9572b8948c431314dc720d81b2145f590e9
joe <joeting919@gmail.com>
2023-01-25 11:00:59 -0700

    edit files

--- Project-1/provider.tf
+++ Project-1/provider.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region                   = var.region
   profile                  = "default"
-  shared_credentials_files = ["C:/Users/Maitri WInd/.aws/credentials"]
+  shared_credentials_files = ["/home/joe/.aws/credentials"]
 }
 
-provider "aws" {
-  region                   = var.region
-  profile                  = "awstest19"
-  alias                    = "awstest19"
-  shared_credentials_files = ["C:/Users/Maitri WInd/.aws/credentialstest19"]
-}
+# provider "aws" {
+#   region                   = var.region
+#   profile                  = "awstest19"
+#   alias                    = "awstest19"
+#   shared_credentials_files = ["C:/Users/Maitri WInd/.aws/credentialstest19"]
+# }
 
